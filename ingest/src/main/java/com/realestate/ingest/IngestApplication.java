78e71532d85d806061cb011153244192a38122c4
sclee <leekun0421@gmail.com>
2026-07-23 09:50:55 +0900

    fix(ingest): exit the process after a run-on-startup batch run completes

--- ingest/src/main/java/com/realestate/ingest/IngestApplication.java
+++ ingest/src/main/java/com/realestate/ingest/IngestApplication.java
@@ -3,11 +3,17 @@ package com.realestate.ingest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableConfigurationProperties(IngestProperties.class)
 public class IngestApplication {
     public static void main(String[] args) {
-        SpringApplication.run(IngestApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(IngestApplication.class, args);
+        boolean runOnStartup = context.getEnvironment()
+            .getProperty("ingest.run-on-startup", Boolean.class, false);
+        if (runOnStartup) {
+            System.exit(SpringApplication.exit(context));
+        }
     }
 }
